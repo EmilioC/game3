7b8dd0db8596d8c89c8b5c21c67b14188f9e853f
EmilioC <emicorgul@gmail.com>
2023-11-26 00:13:29 +0100

    Update sidebar style

--- src/app/gif/components/sidebar/sidebar.component.ts
+++ src/app/gif/components/sidebar/sidebar.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { GifsServicesService } from '../../services/gifs.services.service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './sidebar.component.html'
+  templateUrl: './sidebar.component.html',
+  styleUrl: './app.sidebar.css'
 })
 export class SidebarComponent {
+
+  constructor(private gifsService: GifsServicesService) { }
+
+  get tags(): string[] {
+    return this.gifsService.tagsHistory;
+  }
+
+  searchTag(tag: string): void {
+    this.gifsService.searchTag(tag);
+  }
 
 }
