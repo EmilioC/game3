17024dff49163e54390b8f26e85cf7bfb13e3ea2
EmilioC <emicorgul@gmail.com>
2023-11-25 21:54:16 +0100

    Firs commit

---
+++ src/app/gif/components/card-list/card-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-list.component.html',
+  styleUrl: './card-list.component.css'
+})
+export class CardListComponent {
+
+}
