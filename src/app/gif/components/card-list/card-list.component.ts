445eab7dfc222d0b172ef01fe034e5e99b616a42
EmilioC <emicorgul@gmail.com>
2023-11-25 23:40:35 +0100

    Update card list and home.component

--- src/app/gif/components/card-list/card-list.component.ts
+++ src/app/gif/components/card-list/card-list.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'app-card-list',
@@ -8,5 +9,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './card-list.component.html'
 })
 export class CardListComponent {
+  @Input()
+  public gifs: Gif[] = [];
 
 }
--- src/app/gif/pages/home/home.component.ts
+++ src/app/gif/pages/home/home.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchBoxComponent } from '../../components/search-box/search-box.component';
+import { CardListComponent } from '../../components/card-list/card-list.component';
+import { GifsServicesService } from '../../services/gifs.services.service';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, SearchBoxComponent],
+  imports: [CommonModule, SearchBoxComponent, CardListComponent],
   templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+  constructor(private gifService: GifsServicesService) { }
+
+  get gifs(): Gif[] {
+    return this.gifService.gifList;
+  }
 
 }
