c4204fb195ef27601d7bcd447ecff877495d8b0b
EmilioC <emicorgul@gmail.com>
2023-11-25 22:03:21 +0100

    Udate search-box and card-list

--- src/app/gif/components/search-box/search-box.component.ts
+++ src/app/gif/components/search-box/search-box.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-search-box',
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './search-box.component.html',
-  styleUrl: './search-box.component.css'
+  templateUrl: './search-box.component.html'
 })
 export class SearchBoxComponent {
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+  constructor() {
+  }
+
+  searchTag() {
+    const newTag = this.tagInput.nativeElement.value;
+
+    this.gifService.searchTag(newTag);
+
+    this.tagInput.nativeElement.value = '';
+    console.log({ newTag })
+  }
 
 }
