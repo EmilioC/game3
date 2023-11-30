cec742c1deb4551212a4773aec78fbb294835144
EmilioC <emicorgul@gmail.com>
2023-12-01 00:55:25 +0100

    Update the component game-udemy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './gif/pages/home/home.component';
 import { SidebarComponent } from './gif/components/sidebar/sidebar.component';
 import { GameComponent } from './game/game.component';
+import { game_udemy } from './game-udemy/game-udemy.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule,
-    RouterOutlet, HomeComponent, SidebarComponent, GameComponent],
+    RouterOutlet, HomeComponent, SidebarComponent, GameComponent, game_udemy],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/game-udemy/game-udemy.component.ts
+++ src/app/game-udemy/game-udemy.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,6 +8,134 @@ import { CommonModule } from '@angular/common';
   templateUrl: './game-udemy.component.html',
   styleUrl: './game-udemy.component.css'
 })
-export class GameUdemyComponent {
+export class game_udemy implements AfterViewInit {
+  @ViewChild('canvas1') canvasRef!: ElementRef<HTMLCanvasElement>;
+  private ctx!: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+    const canvas = this.canvasRef.nativeElement;
+    this.ctx = canvas.getContext('2d')!;
+    canvas.width = 1500;
+    canvas.height = 500;
+    // Lógica adicional de dibujo con this.ctx...
+  }
+}
+class InputHandler {
 
 }
+
+class SoundController {
+}
+
+class Shield {
+}
+
+class Projectile {
+}
+
+class Particle {
+}
+
+class Player {
+  game: Game;
+  width: number;
+  height: number;
+  constructor(game: Game) {
+
+    this.game = game;
+    this.width = 120;
+    this.height = 190;
+
+  }
+}
+
+class Game {
+  game: any; // Reemplaza 'any' con un tipo específico si es posible
+  width: number;
+  height: number;
+  x: number;
+  y: number;
+  frameX: number;
+  frameY: number;
+  maxFrame: number;
+  speedY: number;
+  maxSpeed: number;
+  projectiles: any[]; // Reemplaza 'any[]' con un tipo específico si es aplicable
+  image: HTMLImageElement | null;
+  powerUp: boolean;
+  powerUpTimer: number;
+  powerUpLimit: number;
+
+  constructor(game: any) { // Usa un tipo específico para 'game' si es posible
+    this.game = game;
+
+    // Los valores iniciales de estas propiedades deben establecerse en otro lugar
+    this.width = 0;
+    this.height = 0;
+    this.x = 0;
+    this.y = 0;
+    this.frameX = 0;
+    this.frameY = 0;
+    this.maxFrame = 0;
+    this.speedY = 0;
+    this.maxSpeed = 0;
+    this.projectiles = [];
+    this.image = null;
+    this.powerUp = false;
+    this.powerUpTimer = 0;
+    this.powerUpLimit = 0;
+  }
+
+  // Métodos adicionales de Player van aquí
+}
+
+
+
+class Enemy {
+}
+
+class Angler1 {
+}
+
+class Angler2 {
+}
+
+class LuckyFish {
+}
+
+class HiveWhale {
+}
+
+class Drone {
+}
+
+class BulbWhale {
+}
+
+class MoonFish {
+}
+
+class Stalker {
+}
+
+class Razorfin {
+}
+
+class Layer {
+}
+
+class Background {
+}
+
+class Explosion {
+}
+
+class SmokeExplosion {
+}
+
+class FireExplosion {
+}
+
+class UI {
+}
+
