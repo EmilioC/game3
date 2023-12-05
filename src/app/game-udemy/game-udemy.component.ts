d90d2e8f2b1a28d541b3542dd86f4fc8aadedcbe
EmilioC <emicorgul@gmail.com>
2023-12-06 00:17:04 +0100

    Update the debug mode in the enemy class

--- src/app/game-udemy/game-udemy.component.ts
+++ src/app/game-udemy/game-udemy.component.ts
@@ -368,7 +368,10 @@ class Enemy {
   draw(context: any): void {
     if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
     context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
-
+    if (this.game.debug) {
+      context.font = '20px Helvetica';
+      context.fillText(this.lives, this.x, this.y);
+    }
 
     // Dibuja la vida del enemigo
     context.font = '50px Arial';
@@ -528,9 +531,9 @@ class UI {
         message1 = 'La cagaste burt lancaster!';
         message2 = '¿La refinitiva?';
       }
-      context.font = '50px ' + this.fontFamily;
+      context.font = '70px ' + this.fontFamily;
       context.fillText(message1, this.game.width * 0.5, this.game.height * 0.5 - 20);
-      context.font = '2 5px ' + this.fontFamily;
+      context.font = '2 25px ' + this.fontFamily;
       context.fillText(message2, this.game.width * 0.5, this.game.height * 0.5 + 20);
     }
     //amo
