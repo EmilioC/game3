80c27f25d4fb663263634134c1da33278a6d12b2
EmilioC <emicorgul@gmail.com>
2023-12-14 17:45:07 +0100

    Create the Class Obstaculo1

--- src/app/game-udemy/game-udemy.component.ts
+++ src/app/game-udemy/game-udemy.component.ts
@@ -237,6 +237,47 @@ class InputHandler {
 
 }
 
+class Obstaculo1 {
+  game: Game;
+  width: number;
+  height: number;
+  frameX: number;
+  maxFrame: number;
+  image: HTMLElement | null = null;
+  fps: number;
+  timer: number;
+  interval: number;
+  constructor(game: Game) {
+    this.game = game;
+    this.width = this.game.player.width;
+    this.height = this.game.player.height;
+    this.frameX = 0;
+    this.maxFrame = 24;
+    this.image = document.getElementById('shield');
+    this.fps = 60;
+    this.timer = 0;
+    this.interval = 1000 / this.fps;
+  }
+  update(deltaTime: number) {
+    if (this.frameX <= this.maxFrame) {
+      if (this.timer > this.interval) {
+        this.frameX++;
+        this.timer = 0;
+      } else {
+        this.timer += deltaTime;
+      }
+    }
+  }
+  draw(context: any) {
+    context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height,
+      this.game.player.x, this.game.player.y, this.width, this.height);
+  }
+  reset() {
+    this.frameX = 0;
+    this.game.sound.shield();
+  }
+}
+
 class SoundController {
   powerUpSound: HTMLAudioElement;
   powerDownSound: HTMLAudioElement;
